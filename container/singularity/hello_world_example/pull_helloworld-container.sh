fa57297a48f28ab42939ade3b16bd9cd6f5a9ee2
Connor Fuhrman <connorfuhrman@outlook.com>
2021-04-24 13:11:57 -0700

    Added hello-world singularity contianer run on UA HPC. Use pull shell script to pull image and runme.sh to submit via qsub

---
+++ container/singularity/hello_world_example/pull_helloworld-container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+module load singularity
+singularity pull ./hello-world.sif shub://vsoch/hello-world
---
+++ container/singularity/hello_world_example/runme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# File does a simple qsub for the hello_world singularity container
+qsub run_helloWorld-singularity.pbs
